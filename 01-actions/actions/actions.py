3b35fd9376033c2b8f78d0aff3b7ca6a6c4230ab
evamir1968 <im03061968@gmail.com>
2022-03-23 19:38:14 +0100

    cambios

--- 01-actions/actions/actions.py
+++ 01-actions/actions/actions.py
@@ -3,6 +3,7 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import requests
 
 
 class ActionHelloWorld(Action):
@@ -14,6 +15,30 @@ class ActionHelloWorld(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text=f"{dt.datetime.now()}")
+        dispatcher.utter_message(text=f"La hora es: {dt.datetime.now()}")
+
+        return []
+
+
+        'GET' \
+ \
+  -H 'accept: application/json'
+
+  
+
+
+
+
+class ActionEmpleo(Action):
+
+    def name(self) -> Text:
+        return "action_show_time"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        resp = requests.get(  'https://www.zaragoza.es/sede/servicio/oferta-empleo/690?rf=html')
+
+        dispatcher.utter_message(text=f"El empleo disponible es:  {dt.datetime.now()}")
 
         return []
